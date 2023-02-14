80f037102f9e81c2a350c0a56ffb9ab270b710e4
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-02-14 15:50:30 -0300

    Update ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -12,10 +12,10 @@ export default function ErrorPage(){
 
     return(
         <div style={{textAlign: "center", margin: "100px 0px"}}>
-            <img width="180px" height="150px" src="./assets/404.png" alt="Error404Image" />
+            <img width="190px" height="150px" src="./assets/404.png" alt="Error404Image" />
             <h1 style={{margin: "30px 0px"}}>Page Not Found!!</h1>
             <p>The Requested URL was not found on this server.</p>
-            <p>Click <span onClick={BackToHomePage} style={{textDecoration: "underline", color: "blue"}}>Here</span> to go back to the Homepage and try the request again.</p>
+            <p>Click <span onClick={BackToHomePage} style={{textDecoration: "underline", color: "blue", cursor: "pointer"}}>Here</span> to go back to the Homepage and try the request again.</p>
         </div>
     )
 }
